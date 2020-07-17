d22bfc0e059728d407710fb54a54282a8124352e
BuildTools <unconfigured@null.spigotmc.org>
2020-07-17 17:50:09 -0400

    stuff

---
+++ Assignment1/routes.js
@@ -0,0 +1,34 @@
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Username</title></head>');
+    res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  } else if (url === '/users') {
+    res.write('<html>');
+    res.write('<head><title>Welcome!</title></head>');
+    res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>');
+    res.write('</html>');
+    return res.end();
+  } else if (url === '/create-user' && method === 'POST') {
+    const body = [];
+
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log(message);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      res.end();
+    });
+  }
+}
+
+module.exports = requestHandler;
